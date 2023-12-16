9acd5dca62911f455f3750f634086cf7ffcc9d73
usman_rizwan <usmanrizwan771@gmail.com>
2023-12-16 21:52:52 +0500

    updated to  realtime data

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
-// var toolbarOptions = [
-//     ["bold", "italic", "underline", "strike"], // toggled buttons
-//     ["blockquote", "code-block"],
+var toolbarOptions = [
+    ["bold", "italic", "underline", "strike"], // toggled buttons
+    ["blockquote", "code-block"],
 
-//     [{ header: 1 }, { header: 2 }], // custom button values
-//     [{ list: "ordered" }, { list: "bullet" }],
-//     [{ script: "sub" }, { script: "super" }], // superscript/subscript
-//     [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-//     [{ direction: "rtl" }], // text direction
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ script: "sub" }, { script: "super" }], // superscript/subscript
+    [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+    [{ direction: "rtl" }], // text direction
 
-//     [{ size: ["small", false, "large", "huge"] }], // custom dropdown
-//     [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
-//     [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-//     [{ font: [] }],
-//     [{ align: [] }],
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
 
-//     ["clean"], // remove formatting button
-// ];
-
-var quill = new Quill("#editor", {
-    theme: "snow",
-});
+    ["clean"], // remove formatting button
+];
+var quill = new Quill('#editor', {
+    modules: {
+      toolbar: toolbarOptions
+    },
+    theme: 'snow'
+  });
 import {
-    app, collection, addDoc, setDoc, doc, db, serverTimestamp ,getDocs ,onSnapshot ,orderBy ,query ,limit
+    app, collection, addDoc, setDoc, doc, db, serverTimestamp, getDocs, onSnapshot, orderBy, query, limit, deleteDoc,
 } from "/firebase.js";
 
 let title = document.getElementById("title");
 let card = document.getElementById("card");
 let btnText = document.getElementById("btnText");
 let loader = document.getElementById("loader");
+var newDocId; // Declare a variable to store the document ID
 let addBlog = async () => {
     var editorContent = quill.root.innerHTML;
+
+
     if (title.value.trim() && editorContent.trim()) {
         btnText.style.display = "none";
         blogBtn.disabled = true;
@@ -39,25 +44,24 @@ let addBlog = async () => {
             const docRef = await addDoc(collection(db, "blogs"), {
                 title: title.value,
                 blogContent: editorContent,
-                timestamp: serverTimestamp()
+                timestamp: serverTimestamp(),
             });
+
+            newDocId = docRef.id; 
 
             btnText.style.display = "block";
             blogBtn.disabled = false;
             loader.style.display = "none";
-            console.log("Document written with ID: ", docRef.id);
+            console.log("Document written with ID: ", newDocId);
             // console.log(editorContent);
-      
-            title.value = ""
+            title.value = "";
             quill.root.innerHTML = "";
-        }
-        catch (error) {
+        } catch (error) {
             console.log("error ===>", error);
             btnText.style.display = "block";
             blogBtn.disabled = false;
             loader.style.display = "none";
         }
-
     } else {
         const Toast = Swal.mixin({
             toast: true,
@@ -75,45 +79,53 @@ let addBlog = async () => {
             title: "Both title and post fields are required "
         });
     }
+
+
 };
 
 let blogBtn = document.getElementById("blogBtn");
 blogBtn && blogBtn.addEventListener("click", addBlog);
 
-let getBlogData = async () => {
+let getBlogData =  () => {
     card.innerHTML = `<div class="text-center">
      <div class="spinner-border text-primary" role="status" id="loader" style="width: 80px; height: 80px; ">
     <span class="visually-hidden">Loading...</span>
   </div>
   </div> `
- 
-    const q =  query(collection(db, "blogs") , orderBy("timestamp" , "desc"));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      const blogsData = [];
-       card.innerHTML = ""
-      querySnapshot.forEach((doc) => {
-        blogsData.push(doc.data());
+  const q = query(collection(db,"blogs"),orderBy("timestamp" , "desc"));
+  const unsubscribe = onSnapshot(q,(data)=>{
+    card.innerHTML = ""
+    data.docChanges().forEach(change => {
+        // card.innerHTML = ""
+        console.log("This is change" ,change);
         card.innerHTML += ` <div class="card mt-4" >         
-        <div class="card-body">
-        <h4 class="card-title text-capitalize">Title: ${doc.data().title}</h4>
-        <hr/>
-        <p class="card-text">${doc.data().blogContent}</p>
-        <div class="d-flex justify-content-end">
-            <!-- Edit Icon -->
-            <button type="button" class="btn btn-warning mx-2">
-                <i class="fas fa-edit"></i> Edit
-            </button>
+     <div class="card-body">
+         <h4 class="card-title text-capitalize">Title: ${change.doc.data().title}</h4>
+    <hr/>
+        <p class="card-text">${change.doc.data().blogContent}</p>
+    <div class="d-flex justify-content-end">
+    <!-- Edit Icon -->
+         <button type="button" class="btn btn-warning mx-2">
+            <i class="fas fa-edit"></i> Edit
+ </button>
             <!-- Delete Icon -->
-            <button type="button" class="btn btn-danger">
+            <button type="button" onclick="delData('${change.doc.id}')" class="btn btn-danger">
                 <i class="fas fa-trash-alt"></i> Delete
             </button>
-        </div>
+         </div>
         </div>
         </div>`;
-      });
-  
-      console.log(blogsData);
     });
-    
+   
+})
+
+
+
 }
 getBlogData()
+
+let delData = async (id) => {
+    console.log(id);
+    await deleteDoc(doc(db, "blogs", id));
+}
+window.delData = delData;
