693fad74cda9ba223706ad3545acb39649985bda
usman_rizwan <usmanrizwan771@gmail.com>
2023-12-16 14:48:30 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,7 @@ import { getFirestore, collection, addDoc, setDoc ,doc , deleteDoc  ,serverTimes
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7pvJuZOX2O-tZJRfcuhPSUyZDZyi1Wbs",
-  authDomain: "fir-users-auth-fc835.firebaseapp.com",
-  projectId: "fir-users-auth-fc835",
-  storageBucket: "fir-users-auth-fc835.appspot.com",
-  messagingSenderId: "240085089487",
-  appId: "1:240085089487:web:e18821a8d60a10123b4bba",
+
 };
 
 // Initialize Firebase
